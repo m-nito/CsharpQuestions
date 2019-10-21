d81d281b4bc7f65e1e193c064ed8ff2e1281a1e4
m-nito <masamisce@gmail.com>
2019-10-21 17:09:23 +0900

    問題6, 7 を追加

---
+++ CSharpTraining/Answer6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CSharpTraining
+{
+    /// <summary>
+    /// ライト機能インターフェース
+    /// </summary>
+    public interface ILight
+    {
+        bool ChangeLight();
+    }
+
+    /// <summary>
+    /// 回答用クラス
+    /// </summary>
+    public class Answer6 : ILight
+    {
+        public bool ChangeLight()
+        {
+            return false;
+        }
+    }
+}
---
+++ CSharpTraining/Answer7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CSharpTraining
+{
+    public class Answer7
+    {
+        public string GetQueryString(int userId)
+        {
+            return null;
+        }
+    }
+}
